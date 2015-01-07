df233b4cee57ad7d9bbfa677e4ac8f2eedcdd1b4
Andy Duncan <ajduncan@gmail.com>
2015-01-07 14:10:04 -0500

    Sorta-kinda working example of oauth2 client/server interaction with anicca.

--- bardo/app.js
+++ bardo/app.js
@@ -19,6 +19,40 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.get('/auth', function (req, res) {
+    var token;
+    var credentials = {
+        clientID: 'thom',
+        clientSecret: 'nightworld',
+        site: 'http://10.150.150.1:9001'
+    };
+
+    var oauth2 = require('simple-oauth2')(credentials);
+
+    oauth2.password.getToken({        
+        username: 'thomseddon',
+        password: 'nightworld'
+    }, saveToken);
+
+    function saveToken(error, result) {
+        if (error) { 
+            console.log('Access Token Error', error.message); 
+        } else {
+            token = oauth2.accessToken.create(result);
+            res.send(token);
+        }
+        res.send('DERP');
+    };
+});
+
+app.get('/viewer', function (req, res) {
+    res.render('viewer');
+});
+
 app.listen(8000, function() {
   console.log('Bardo server running and listening on port 8000');
 })
